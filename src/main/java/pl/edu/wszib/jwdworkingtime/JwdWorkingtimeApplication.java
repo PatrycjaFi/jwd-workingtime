0ccfb1b8d32e993e7269d4f77089e950aabbfc7a
Patrycja <filipekpatrycjaa@gmail.com>
2022-06-17 11:40:25 +0200

    Initial commit

---
+++ src/main/java/pl/edu/wszib/jwdworkingtime/JwdWorkingtimeApplication.java
@@ -0,0 +1,13 @@
+package pl.edu.wszib.jwdworkingtime;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JwdWorkingtimeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JwdWorkingtimeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/edu/wszib/jwdworkingtime/JwdWorkingtimeApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.edu.wszib.jwdworkingtime;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JwdWorkingtimeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

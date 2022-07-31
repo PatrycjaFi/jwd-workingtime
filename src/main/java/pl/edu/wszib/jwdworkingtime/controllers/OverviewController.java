c07c582294d74271173e0ca651c3f765482fc8c5
Patrycja <filipekpatrycjaa@gmail.com>
2022-07-31 12:26:54 +0200

    Date log page creating.

--- src/main/java/pl/edu/wszib/jwdworkingtime/controllers/OverviewController.java
+++ src/main/java/pl/edu/wszib/jwdworkingtime/controllers/OverviewController.java
@@ -5,7 +5,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import pl.edu.wszib.jwdworkingtime.services.SelectedAbsenceReasonService;
-import pl.edu.wszib.jwdworkingtime.services.SelectedDateService;
 import pl.edu.wszib.jwdworkingtime.services.SelectedEmployeeService;
 
 @Controller
@@ -14,12 +13,12 @@ public class OverviewController {
     @Autowired
     SelectedAbsenceReasonService selectedAbsenceReasonService;
     SelectedEmployeeService selectedEmployeeService;
-    SelectedDateService selectedDateService;
 
-    public OverviewController(SelectedAbsenceReasonService selectedAbsenceReasonService, SelectedEmployeeService selectedEmployeeService, SelectedDateService selectedDateService) {
+
+    public OverviewController(SelectedAbsenceReasonService selectedAbsenceReasonService, SelectedEmployeeService selectedEmployeeService) {
         this.selectedAbsenceReasonService = selectedAbsenceReasonService;
         this.selectedEmployeeService = selectedEmployeeService;
-        this.selectedDateService = selectedDateService;
+
     }
 
     @GetMapping("/overview")
@@ -27,7 +26,7 @@ public class OverviewController {
 
         model.addAttribute("selectedAbsenceReasons", selectedAbsenceReasonService.getAllData());
         model.addAttribute("selectedEmployees", selectedEmployeeService.getAllData());
-        model.addAttribute("selectedDates", selectedDateService.getAllData());
+
 
         return "overviewPage";
     }
--- src/main/java/pl/edu/wszib/jwdworkingtime/controllers/SelectedDatePageController.java
+++ src/main/java/pl/edu/wszib/jwdworkingtime/controllers/SelectedDatePageController.java
@@ -1,21 +1,22 @@
 package pl.edu.wszib.jwdworkingtime.controllers;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import pl.edu.wszib.jwdworkingtime.services.SelectedDateService;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import pl.edu.wszib.jwdworkingtime.model.SelectDateModel;
+
 
 @Controller
+@RequestMapping
 public class SelectedDatePageController {
 
-    @Autowired
-    SelectedDateService selectedDateService;
+    @GetMapping("/date-log")
+    public String showDatePage(@ModelAttribute("selectDate") SelectDateModel selectDateModel) {
 
-
-    @GetMapping("date-log")
-    public String showDatePage(String date) {
-
-        selectedDateService.save(date);
         return "dateLogPage";
     }
-}
+
+    }
+

cf0ac10c465464dcbb074100d841c76b28e72262
Patrycja <filipekpatrycjaa@gmail.com>
2022-07-31 17:53:57 +0200

    correction

--- src/main/java/pl/edu/wszib/jwdworkingtime/JwdWorkingtimeApplication.java
+++ src/main/java/pl/edu/wszib/jwdworkingtime/JwdWorkingtimeApplication.java
@@ -4,8 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.PropertySource;
 
+
 @SpringBootApplication
-//@PropertySource("classpath:db.properties")
+@PropertySource("classpath:db.properties")
 public class JwdWorkingtimeApplication {
 
 	public static void main(String[] args) {
